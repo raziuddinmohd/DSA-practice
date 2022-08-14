e429e3d51c3bd07b13d7d9fcf5703fcb738dca2c
raziuddinmohd <raziuddin.mohd1994@gmail.com>
2022-08-14 14:40:12 +0530

    insert First operation tested

--- DSA_algorithm/Program.cs
+++ DSA_algorithm/Program.cs
@@ -4,19 +4,31 @@ using DSA_algorithm.LinkedList;
 Console.WriteLine("Hello, World!");
 
 
-Node NodeA = new Node();
-NodeA.Data = 4665;
+//Node NodeA = new Node();
+//NodeA.Data = 4665;
 
-Node NodeB= new Node();
-NodeB.Data = 7897;
+//Node NodeB= new Node();
+//NodeB.Data = 7897;
 
-Node NodeC=new Node();
-NodeC.Data = 879;
+//Node NodeC=new Node();
+//NodeC.Data = 879;
 
-Node NodeD=new Node();
-NodeD.Data = 324234;
+//Node NodeD=new Node();
+//NodeD.Data = 324234;
 
 
-NodeA.Next = NodeB;
-NodeB.Next = NodeC;
-NodeC.Next = NodeD;
+//NodeA.Next = NodeB;
+//NodeB.Next = NodeC;
+//NodeC.Next = NodeD;
+
+
+LinkedListUtility LinkedListUtility = new LinkedListUtility();
+LinkedListUtility.InsertFirst(12);
+LinkedListUtility.InsertFirst(13);
+
+LinkedListUtility.InsertFirst(14);
+LinkedListUtility.InsertFirst(15);
+
+LinkedListUtility.InsertFirst(16);
+
+Console.WriteLine("final execution");
