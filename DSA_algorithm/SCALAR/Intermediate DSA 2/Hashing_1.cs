d60814ddfc98b494a35a974908c38bc2a0663459
raziuddinmohd <raziuddin.mohd1994@gmail.com>
2023-07-08 16:32:13 +0530

    Q5. Count distinct elements https://www.scaler.com/academy/mentee-dashboard/class/45629/assignment/problems/27502?navref=cl_tt_nv

--- DSA_algorithm/SCALAR/Intermediate DSA 2/Hashing_1.cs
+++ DSA_algorithm/SCALAR/Intermediate DSA 2/Hashing_1.cs
@@ -206,5 +206,49 @@ namespace DSA_algorithm.SCALAR.Intermediate_DSA_2
         //var item = new List<int> { -1, 1 };
         //var res = DSA_algorithm.SCALAR.Intermediate_DSA_2.Hashing_1.checkIfSubArrayWithSumZeroExist(item);
         #endregion
+
+        #region return count of  unique number of elements in an array
+        /*
+         Problem Description
+        Given an array A of N integers, return the number of unique elements in the array.
+        
+        
+        Problem Constraints
+        1 <= N <= 105
+        1 <= A[i] <= 109
+        
+        Input Format
+        First argument A is an array of integers.
+        
+        
+        Output Format
+        Return an integer.
+        
+        
+        Example Input
+        Input 1:
+        A = [3, 4, 3, 6, 6]
+        Input 2:
+        A = [3, 3, 3, 9, 0, 1, 0]
+        
+        
+        Example Output
+        Output 1:
+        3
+        Output 2:
+        4
+         */
+
+        public static int countOfUniqueInArray(List<int> A)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < A.Count; i++)
+            {
+                set.Add(A[i]);
+            }
+
+            return set.Count;
+        }
+        #endregion
     }
 }
